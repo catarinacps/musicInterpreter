7e4d054c161c7c8a4be3983faf5c09c12682f9d0
dieMaus <riquerosario@hotmail.com>
2016-11-11 19:31:01 -0200

    me
    mes

---
+++ musicInterpreter/src/musicinterpreter/Musica.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package musicinterpreter;
+
+/**
+ *
+ * @author Luiz Eduardo
+ */
+public class Musica {
+    private int volume;
+    private int ritmo;
+    private String instrumento;
+    private Nota[] notas;
+    
+    public Musica(int volume, int ritmo, String instrumento, String nomeArquivo){
+        
+    }
+    
+    private Nota[] leitorArquivo(nomeArquivo){
+        
+    }
+}
