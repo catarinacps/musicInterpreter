de39127d77f763e3ebd1f57a1a3c99e86b2cb0e8
dieMaus <riquerosario@hotmail.com>
2016-11-11 20:23:38 -0200

    chchchchchanges

--- musicInterpreter/src/musicinterpreter/LeitorDecodificador.java
+++ musicInterpreter/src/musicinterpreter/LeitorDecodificador.java
@@ -17,16 +17,31 @@ public class LeitorDecodificador {
             FileInputStream arquivoEntrada = new FileInputStream(nomeArquivo);
             InputStreamReader arquivoFormatado = new InputStreamReader(arquivoEntrada);
             
+            long tamanho = arquivoEntrada.getChannel().size();
+            
+            Nota[] saidaDecoficada = new Nota[tamanho];
+            
+            int i = 0;
+            
             do{
                 int buffer = arquivoFormatado.read();
                 
+                saidaDecoficada[i] = decodificaEntrada((char) buffer);
                 //faz alguma coisa com o buffer, tipo decodificar sla memes
             }while(buffer != -1);
             
-            arquivo.close();
+            arquivoEntrada.close();
+            
+            return saidaDecodificada;
         }
         catch(IOException erro){
             System.err.printf("Erro na abertura do arquivo %s.\n", erro.getMessage());
+            return null;
         }
+        
+    }
+    
+    private void decodificaEntrada(char entradaChar){
+        
     }
 }
